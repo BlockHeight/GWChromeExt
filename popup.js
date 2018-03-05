596e307c0eaf6d53fc01513bb22f57d080263b31
Frank Fernandes <frankfernandes@gmail.com>
2018-03-04 22:34:56 -0800

    phase 0

--- popup.js
+++ popup.js
@@ -1,13 +1,46 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+document.addEventListener('DOMContentLoaded', function () {
+		document.querySelector('button').addEventListener('click', main);      
+		var os = '';
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback called when the URL of the current tab
- *   is found.
- */
+		chrome.runtime.getPlatformInfo(function(info) {
+				os = info.os;
+				if ( os == 'mac'){
+						document.getElementById("client_link").href = 'https://s3.ap-south-1.amazonaws.com/miner-bucket/client/latest/darwin/gc-client_latest_x64.dmg';
+				} else if (os == 'win'){
+						document.getElementById("client_link").href = 'https://s3.ap-south-1.amazonaws.com/miner-bucket/client/latest/windows/gc-client_latest-x64.exe';
+				} else if (os == 'linux'){
+						document.getElementById("client_link").href = 'https://s3.ap-south-1.amazonaws.com/miner-bucket/client/latest/linux/gc-client_latest-amd64.deb';				
+				}
+		});
+});
+
+function main() {
+var source = document.getElementById('source').value;
+chrome.runtime.getPlatformInfo(function(info) {
+document.getElementById("result").innerHTML = info.os;
+});
+	//if(info.os == 'mac'){
+	
+	//} else if (info.os == 'win'){
+//document.getElementById("result").innerHTML = info.os;
+	
+//	} else if (info.os == 'linux'){
+	
+//	}
+
+}
+
+
+/*function returnOS(){
+alert(1);
+	chrome.runtime.getPlatformInfo(function(info) {
+    // Display host OS in the console
+    alert(1);
+	  console.log(info.os);
+    return (info.os);
+  });
+}*/
+
 function getCurrentTabUrl(callback) {
   // Query filter to be passed to chrome.tabs.query - see
   // https://developer.chrome.com/extensions/tabs#method-query
@@ -46,81 +79,3 @@ function getCurrentTabUrl(callback) {
   // });
   // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
-
-/**
- * Change the background color of the current page.
- *
- * @param {string} color The new background color.
- */
-function changeBackgroundColor(color) {
-  var script = 'document.body.style.backgroundColor="' + color + '";';
-  // See https://developer.chrome.com/extensions/tabs#method-executeScript.
-  // chrome.tabs.executeScript allows us to programmatically inject JavaScript
-  // into a page. Since we omit the optional first argument "tabId", the script
-  // is inserted into the active tab of the current window, which serves as the
-  // default.
-  chrome.tabs.executeScript({
-    code: script
-  });
-}
-
-/**
- * Gets the saved background color for url.
- *
- * @param {string} url URL whose background color is to be retrieved.
- * @param {function(string)} callback called with the saved background color for
- *     the given url on success, or a falsy value if no color is retrieved.
- */
-function getSavedBackgroundColor(url, callback) {
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
-  // for chrome.runtime.lastError to ensure correctness even when the API call
-  // fails.
-  chrome.storage.sync.get(url, (items) => {
-    callback(chrome.runtime.lastError ? null : items[url]);
-  });
-}
-
-/**
- * Sets the given background color for url.
- *
- * @param {string} url URL for which background color is to be saved.
- * @param {string} color The background color to be saved.
- */
-function saveBackgroundColor(url, color) {
-  var items = {};
-  items[url] = color;
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We omit the
-  // optional callback since we don't need to perform any action once the
-  // background color is saved.
-  chrome.storage.sync.set(items);
-}
-
-// This extension loads the saved background color for the current tab if one
-// exists. The user can select a new background color from the dropdown for the
-// current page, and it will be saved as part of the extension's isolated
-// storage. The chrome.storage API is used for this purpose. This is different
-// from the window.localStorage API, which is synchronous and stores data bound
-// to a document's origin. Also, using chrome.storage.sync instead of
-// chrome.storage.local allows the extension data to be synced across multiple
-// user devices.
-document.addEventListener('DOMContentLoaded', () => {
-  getCurrentTabUrl((url) => {
-    var dropdown = document.getElementById('dropdown');
-
-    // Load the saved background color for this page and modify the dropdown
-    // value, if needed.
-    getSavedBackgroundColor(url, (savedColor) => {
-      if (savedColor) {
-        changeBackgroundColor(savedColor);
-        dropdown.value = savedColor;
-      }
-    });
-
-    // Ensure the background color is changed and saved when the dropdown
-    // selection changes.
-    dropdown.addEventListener('change', () => {
-      changeBackgroundColor(dropdown.value);
-      saveBackgroundColor(url, dropdown.value);
-    });
-  });
-});
